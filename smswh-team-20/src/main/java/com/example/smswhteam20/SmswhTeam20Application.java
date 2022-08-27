c2ae001f88c537a2f3a5f6d1b028b6ffdb29d5c2
5jisoo <56earls@gmail.com>
2022-08-27 09:54:43 +0900

    setting

---
+++ smswh-team-20/src/main/java/com/example/smswhteam20/SmswhTeam20Application.java
@@ -0,0 +1,13 @@
+package com.example.smswhteam20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmswhTeam20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmswhTeam20Application.class, args);
+	}
+
+}
---
+++ smswh-team-20/src/test/java/com/example/smswhteam20/SmswhTeam20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smswhteam20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmswhTeam20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
