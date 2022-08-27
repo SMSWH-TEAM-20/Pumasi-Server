f023c1b8d1c8c3c2642b72797d389268d8570a2f
dong0one <dong0one9659@sookmyung.ac.kr>
2022-08-27 11:50:17 +0900

    domain, repo

---
+++ smswh-team-20/src/main/java/com/example/smswhteam20/repository/RegistrationRepository.java
@@ -0,0 +1,11 @@
+package com.example.smswhteam20.repository;
+
+import com.example.smswhteam20.domain.Registration;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RegistrationRepository extends CrudRepository<Registration, Integer> {
+    // save -> 등록
+
+    //delete -> 삭제
+
+}
